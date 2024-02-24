73492704e97c5fd62dbd67ce98efbd4be13320da
Cameron Eldridge <cameldridge@gmail.com>
2024-02-24 14:39:54 -0500

    pull the google sheet

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use serenity::prelude::*;
+use sheets::types::{DateTimeRenderOption, Dimension, GridProperties, ValueRenderOption};
 
 mod graphql;
 mod shopify;
@@ -13,13 +14,48 @@ struct Data {
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
+fn sheet_id() -> String {
+    std::env::var("SHEETS_SHEET_ID").unwrap()
+}
+
 #[poise::command(slash_command, prefix_command)]
 async fn pull(
     ctx: Context<'_>,
     #[description = "Order Number"] order_number: OrderNumber,
 ) -> Result<(), Error> {
-    let order = ctx.data().shopify.get_order(order_number).await?;
-    // Confirm number of pulls
+    let data = ctx.data();
+    let order = data.shopify.get_order(order_number).await?;
+    let pulls = order.line_items.len(); // TODO: Check for matching SKUs
+
+    let spreadsheet = data
+        .sheets
+        .spreadsheets()
+        .get(&sheet_id(), true, &[])
+        .await?
+        .body;
+    let grid_properties = spreadsheet.sheets[0]
+        .properties
+        .as_ref()
+        .unwrap()
+        .grid_properties
+        .as_ref()
+        .unwrap();
+    let column_headers = data
+        .sheets
+        .spreadsheets()
+        .values_get(
+            &sheet_id(),
+            &format!("A1:A{}", grid_properties.column_count),
+            DateTimeRenderOption::Noop,
+            Dimension::Columns,
+            ValueRenderOption::Noop,
+        )
+        .await?
+        .body;
+    let order_number_column = column_headers.values[0].iter().position(|x| x == "Order Number");
+    let discord_user_column = column_headers.values[0].iter().position(|x| x == "Discord User");
+    let pulls_column = column_headers.values[0].iter().position(|x| x == "Pulls");
+
     // Save pull state in database (Google Sheet?)
     // Create the Discord post for the pull, with options buttons
 
