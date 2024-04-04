4f8b17f4c858c0dec3566eb83d8f8315283b3e7d
Cameron Eldridge <cameldridge@gmail.com>
2024-04-03 21:01:55 -0400

    replace banner image with custom image from pearl

--- src/config/banner.rs
+++ src/config/banner.rs
@@ -1,9 +1,14 @@
 use super::{Pool, Product};
 use image::imageops::{overlay, FilterType};
-use image::{load_from_memory, ImageError};
+use image::{load_from_memory_with_format, ImageError, ImageFormat};
 use serde::{Deserialize, Serialize};
 
 const SUMMON_PNG: &[u8] = include_bytes!("../../assets/summon.png");
+const RED_PNG: &[u8] = include_bytes!("../../assets/red.png");
+const BLUE_PNG: &[u8] = include_bytes!("../../assets/blue.png");
+const GREEN_PNG: &[u8] = include_bytes!("../../assets/green.png");
+const WHITE_PNG: &[u8] = include_bytes!("../../assets/grey.png");
+
 const ROBIN_PNG: &[u8] = include_bytes!("../../assets/Robin.png");
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -12,17 +17,38 @@ pub struct Banner {
     pub pulls: Vec<Option<Product>>,
 }
 
+impl Pool {
+    fn image(&self) -> &'static [u8] {
+        match self {
+            Pool::Red => RED_PNG,
+            Pool::Blue => BLUE_PNG,
+            Pool::Green => GREEN_PNG,
+            Pool::White => WHITE_PNG,
+        }
+    }
+}
+
+const X: [i64; 5] = [500, 780, 710, 295, 205];
+const Y: [i64; 5] = [210, 400, 775, 775, 400];
+const SIZE: i64 = 192;
+
 impl Banner {
     pub fn pulled(&self) -> usize {
         self.pulls.iter().filter(|s| s.is_some()).count()
     }
 
     pub fn to_image(&self) -> Result<Vec<u8>, ImageError> {
-        let mut summon = load_from_memory(SUMMON_PNG).unwrap();
-        let robin = load_from_memory(ROBIN_PNG)
+        let mut summon = load_from_memory_with_format(SUMMON_PNG, ImageFormat::Png).unwrap();
+
+        let robin = load_from_memory_with_format(ROBIN_PNG, ImageFormat::Png)
             .unwrap()
             .resize(180, 180, FilterType::Triangle);
-        overlay(&mut summon, &robin, 270, 90);
+
+        for (i, pool) in self.pools.iter().enumerate() {
+            let image = load_from_memory_with_format(pool.image(), ImageFormat::Png).unwrap();
+            overlay(&mut summon, &image, X[i] - SIZE / 2, Y[i] - SIZE / 2);
+        }
+
         let mut bytes: Vec<u8> = Vec::new();
         summon.write_to(
             &mut std::io::Cursor::new(&mut bytes),
