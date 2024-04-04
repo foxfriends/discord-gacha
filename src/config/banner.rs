995c1d9019b3501c68c62a8965060f26f7759e4e
Cameron Eldridge <cameldridge@gmail.com>
2024-04-03 21:30:57 -0400

    more assets changes

--- src/config/banner.rs
+++ src/config/banner.rs
@@ -1,15 +1,16 @@
 use super::{Pool, Product};
-use image::imageops::{overlay, FilterType};
-use image::{load_from_memory_with_format, ImageError, ImageFormat};
+use image::imageops::overlay;
+use image::{load, load_from_memory_with_format, ImageError, ImageFormat};
 use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
 
 const SUMMON_PNG: &[u8] = include_bytes!("../../assets/summon.png");
 const RED_PNG: &[u8] = include_bytes!("../../assets/red.png");
 const BLUE_PNG: &[u8] = include_bytes!("../../assets/blue.png");
 const GREEN_PNG: &[u8] = include_bytes!("../../assets/green.png");
 const WHITE_PNG: &[u8] = include_bytes!("../../assets/grey.png");
-
-const ROBIN_PNG: &[u8] = include_bytes!("../../assets/Robin.png");
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Banner {
@@ -30,7 +31,6 @@ impl Pool {
 
 const X: [i64; 5] = [500, 780, 710, 295, 205];
 const Y: [i64; 5] = [210, 400, 775, 775, 400];
-const SIZE: i64 = 192;
 
 impl Banner {
     pub fn pulled(&self) -> usize {
@@ -40,13 +40,36 @@ impl Banner {
     pub fn to_image(&self) -> Result<Vec<u8>, ImageError> {
         let mut summon = load_from_memory_with_format(SUMMON_PNG, ImageFormat::Png).unwrap();
 
-        let robin = load_from_memory_with_format(ROBIN_PNG, ImageFormat::Png)
-            .unwrap()
-            .resize(180, 180, FilterType::Triangle);
-
         for (i, pool) in self.pools.iter().enumerate() {
             let image = load_from_memory_with_format(pool.image(), ImageFormat::Png).unwrap();
-            overlay(&mut summon, &image, X[i] - SIZE / 2, Y[i] - SIZE / 2);
+            overlay(
+                &mut summon,
+                &image,
+                X[i] - image.width() as i64 / 2,
+                Y[i] - image.height() as i64 / 2,
+            );
+        }
+
+        let assets_dir: PathBuf = "./assets".parse().unwrap();
+        for (i, pull) in self.pulls.iter().enumerate() {
+            if let Some(pull) = pull {
+                // let Ok(file) = File::open(assets_dir.join(&pull.sku).with_extension("png")) else {
+                //     continue;
+                // };
+                let Ok(file) = File::open(assets_dir.join("EPfea1A").with_extension("png")) else {
+                    continue;
+                };
+                let reader = BufReader::new(file);
+                let Ok(image) = load(reader, ImageFormat::Png) else {
+                    continue;
+                };
+                overlay(
+                    &mut summon,
+                    &image,
+                    X[i] - image.width() as i64 / 2,
+                    Y[i] - image.height() as i64 / 2,
+                );
+            }
         }
 
         let mut bytes: Vec<u8> = Vec::new();
